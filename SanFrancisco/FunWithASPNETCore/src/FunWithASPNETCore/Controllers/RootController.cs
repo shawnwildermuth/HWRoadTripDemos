423d2fa019009ef6551185d5a2acf77e764c9ee6
Shawn Wildermuth <shawn@wildermuth.com>
2016-03-08 18:38:16 -0800

    San Francisco

---
+++ SanFrancisco/FunWithASPNETCore/src/FunWithASPNETCore/Controllers/RootController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace FunWithASPNETCore.Controllers
+{
+  [Route("root")]
+  public class RootController : Controller
+  {
+    [HttpGet("index/{page:int?}")]
+    public IActionResult Index(int page = 0)
+    {
+      ViewBag.Title = "Home Page";
+
+      throw new InvalidOperationException();
+
+      return View();
+    }
+
+    [HttpGet("contact")]
+    public IActionResult Contact()
+    {
+      return View();
+    }
+  }
+}
